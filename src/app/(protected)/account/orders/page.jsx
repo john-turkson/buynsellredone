7ab1d8594934e780a78c37072d46b1041299d37a
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-11-23 16:16:04 -0500

    template out individual item layout in order

--- src/app/(protected)/account/orders/page.jsx
+++ src/app/(protected)/account/orders/page.jsx
@@ -1,3 +1,18 @@
 export default function AccountOrders() {
-	return <div>AccountOrders</div>;
+	return (
+		<div className="flex items-center justify-center">
+			<div className="flex flex-col items-center space-y-4">
+				<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+					<div className="row-span-3">Image</div>
+					<div className="font-bold">Title</div>
+					<div>$Price</div>
+				</div>
+				<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+					<div className="row-span-3">Image</div>
+					<div className="font-bold">Title</div>
+					<div>$Price</div>
+				</div>
+			</div>
+		</div>
+	);
 }
