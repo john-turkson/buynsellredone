124be095abafe7510c934d63f566d14652b303eb
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-11-23 18:50:33 -0500

    add Order ID and Amount

--- src/app/(protected)/account/orders/page.jsx
+++ src/app/(protected)/account/orders/page.jsx
@@ -2,15 +2,19 @@ export default function AccountOrders() {
 	return (
 		<div className="flex items-center justify-center">
 			<div className="flex flex-col items-center space-y-4">
-				<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-					<div className="row-span-3">Image</div>
-					<div className="font-bold">Title</div>
-					<div>$Price</div>
-				</div>
-				<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-					<div className="row-span-3">Image</div>
-					<div className="font-bold">Title</div>
-					<div>$Price</div>
+				<div className="flex flex-col items-center space-y-4">
+					<div>Order ID</div>
+					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+						<div className="row-span-3">Image</div>
+						<div className="font-bold">Title</div>
+						<div>$Price</div>
+					</div>
+					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+						<div className="row-span-3">Image</div>
+						<div className="font-bold">Title</div>
+						<div>$Price</div>
+					</div>
+					<div className="font-bold">Order Amount</div>
 				</div>
 			</div>
 		</div>
