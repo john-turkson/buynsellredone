80cbca8fd1cbf3853501ed0ef71091c10fbfc116
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-11-25 11:49:13 -0500

    add temp image to up prototype

--- src/app/(protected)/account/orders/page.jsx
+++ src/app/(protected)/account/orders/page.jsx
@@ -1,20 +1,23 @@
+import Image from "next/image";
+
 export default function AccountOrders() {
 	return (
 		<div className="flex items-center justify-center">
 			<div className="flex flex-col items-center space-y-4">
-				<div className="flex flex-col items-center space-y-4">
-					<div>Order ID</div>
+				<div className="flex flex-col items-center space-y-4 border-2 p-2">
+					<div className="text-left">Order Date</div>
 					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-						<div className="row-span-3">Image</div>
+						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
 						<div className="font-bold">Title</div>
 						<div>$Price</div>
 					</div>
 					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-						<div className="row-span-3">Image</div>
+						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
 						<div className="font-bold">Title</div>
 						<div>$Price</div>
 					</div>
-					<div className="font-bold">Order Amount</div>
+					<div className="font-bold text-left">Order Amount</div>
+					<div className="text-left">Status</div>
 				</div>
 			</div>
 		</div>
