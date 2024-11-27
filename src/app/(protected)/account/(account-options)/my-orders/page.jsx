9a7eb9a494230f0f872cacd49859729ea21aa9a5
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-11-27 14:19:08 -0500

    move orders page to new route

--- src/app/(protected)/account/(account-options)/my-orders/page.jsx
+++ src/app/(protected)/account/(account-options)/my-orders/page.jsx
@@ -1,6 +1,41 @@
+import Image from "next/image";
 
-export default function MyOrders() {
-  return (
-    <div>My Orders</div>
-  )
+async function fetchOrders() {
+	try {
+		const response = await axios.get("/api/get-orders", {
+			params: { userId: loggedInUserData.userId },
+		});
+		console.log("Response Data: " + response.data);
+		return response.data.orders || {}; // Return the orders
+	} catch (error) {
+		console.error("Error fetching orders:", error);
+		return null; // Return null on failure
+	}
+}
+
+export default async function MyOrders({ params }) {
+	const orders = await fetchOrders();
+	console.log("Orders: " + orders);
+
+	return (
+		<div className="flex items-center justify-center">
+			<div className="flex flex-col items-center space-y-4">
+				<div className="flex flex-col items-center space-y-4 border-2 p-2">
+					<div className="text-left">Order Date</div>
+					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
+						<div className="font-bold">Title</div>
+						<div>$Price</div>
+					</div>
+					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
+						<div className="font-bold">Title</div>
+						<div>$Price</div>
+					</div>
+					<div className="font-bold text-left">Order Amount</div>
+					<div className="text-left">Status</div>
+				</div>
+			</div>
+		</div>
+	);
 }
