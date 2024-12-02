0aabeb45f3c984d1b1911c6430f91db65c71e8f4
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-12-01 21:10:42 -0500

    start displaying pulled data on page

--- src/app/(protected)/account/(account-options)/my-orders/page.jsx
+++ src/app/(protected)/account/(account-options)/my-orders/page.jsx
@@ -13,7 +13,6 @@ async function fetchOrders(userID) {
 			params: { userId: userID },
 			headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" },
 		});
-		console.log("Response Data: " + response.data);
 		return response.data.orders || {}; // Return the orders
 	} catch (error) {
 		console.error("Error fetching orders:", error);
@@ -24,25 +23,19 @@ async function fetchOrders(userID) {
 export default async function MyOrders() {
 	const session = await auth();
 	const orders = await fetchOrders(session?.user.userId);
-	console.log("Orders: " + orders);
 
 	return (
 		<div className="flex items-center justify-center">
 			<div className="flex flex-col items-center space-y-4">
 				<div className="flex flex-col items-center space-y-4 border-2 p-2">
-					<div className="text-left">Order Date</div>
+					<div className="text-left">{orders[0].orderDate.substr(0, 10)}</div>
 					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
 						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
 						<div className="font-bold">Title</div>
 						<div>$Price</div>
 					</div>
-					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-						<Image src="https://http.cat/522.jpg" alt="Image" width={200} height={200} className="row-span-2" />
-						<div className="font-bold">Title</div>
-						<div>$Price</div>
-					</div>
-					<div className="font-bold text-left">Order Amount</div>
-					<div className="text-left">Status</div>
+					<div className="font-bold text-left">Total: ${orders[0].totalAmount}</div>
+					<div className="text-left">Status: {orders[0].status}</div>
 				</div>
 			</div>
 		</div>
