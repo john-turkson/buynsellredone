e4547904f9f4f8097121d624418c8a22f98813a3
LameboiHari <60195485+LameboiHari@users.noreply.github.com>
2024-12-02 14:54:29 -0500

    Fixed Shipping Dropdown Issue

--- src/app/(protected)/cart/components/ShippingDropdown.jsx
+++ src/app/(protected)/cart/components/ShippingDropdown.jsx
@@ -15,12 +15,16 @@ export default function ShippingDropdown({ onCountryChange }) {
     return (
         <select
             className="pe-9 block w-24 border-gray-200 rounded-lg text-sm focus:border-purple-500 focus:ring-purple-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            value={selectedCountry}
+            defaultValue=""
             onChange={handleChange}
         >
+            <option value="" disabled hidden>
+                Select
+            </option>
             <option value="CA">CA</option>
             <option value="US">US</option>
             <option value="GB">GB</option>
         </select>
+
     );
 }
