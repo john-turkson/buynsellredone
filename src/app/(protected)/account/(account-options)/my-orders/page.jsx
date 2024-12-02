f7a04d9534739afe127c10108c204a7306bf7a3c
Obspogon <72287594+Obspogon@users.noreply.github.com>
2024-12-01 21:44:14 -0500

    dynamically get number of listings in object

--- src/app/(protected)/account/(account-options)/my-orders/page.jsx
+++ src/app/(protected)/account/(account-options)/my-orders/page.jsx
@@ -40,18 +40,23 @@ export default async function MyOrders() {
 	const session = await auth();
 	const orders = await fetchOrders(session?.user.userId);
 	const listings = await getListings(orders[0].listings);
-	console.log(listings[0].images[0]);
+	let listingCards = [];
+	for (let i = 0; i < listings.length; i++) {
+		listingCards.push(
+			<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
+				<Image src={listings[i].images[0]} alt="Image" width={200} height={200} className="row-span-2" />
+				<div className="font-bold">{listings[i].name}</div>
+				<div>${listings[i].price}</div>
+			</div>
+		);
+	}
 
 	return (
 		<div className="flex items-center justify-center">
 			<div className="flex flex-col items-center space-y-4">
 				<div className="flex flex-col items-center space-y-4 border-2 p-2">
 					<div className="text-left">{orders[0].orderDate.substr(0, 10)}</div>
-					<div className="grid grid-cols-2 grid-rows-2 gap-4 border-2 rounded p-2">
-						<Image src={listings[0].images[0]} alt="Image" width={200} height={200} className="row-span-2" />
-						<div className="font-bold">{listings[0].name}</div>
-						<div>${listings[0].price}</div>
-					</div>
+					{listingCards}
 					<div className="font-bold text-left">Total: ${orders[0].totalAmount}</div>
 					<div className="text-left">Status: {orders[0].status}</div>
 				</div>
